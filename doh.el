ac8d4df04a02516ad2c666f069f9045d4d42a091
Noah Friedman <friedman@splode.com>
2015-01-11 03:11:28 +0100

    updates

--- doh.el
+++ doh.el
@@ -1,26 +1,11 @@
-;;; doh.el
-
-;; Copyright (C) 1996, 2000 Noah S. Friedman
+;;; doh.el --- D'oh!
 
 ;; Author: Noah Friedman <friedman@splode.com>
 ;; Created: 1996-06-24
+;; Keywords: games
+;; License: public domain
 
-;; $Id: doh.el,v 1.3 2000/03/13 11:13:39 friedman Exp $
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
-;; any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, you can either send email to this
-;; program's maintainer or write to: The Free Software Foundation,
-;; Inc.; 59 Temple Place, Suite 330; Boston, MA 02111-1307, USA.
+;; $Id: doh.el,v 1.4 2013/08/03 17:22:02 friedman Exp $
 
 (require 'advice)
 
